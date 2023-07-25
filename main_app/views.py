ba91b4d77732a6f15e9b231267e14045cdc91927
austinpowers99 <samanthaf_9@outlook.com>
2023-07-25 16:25:15 -0700

    about section done

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def coins_index(request):
+    return render(request, 'coins/index.html', {
+        'coins': coins
+})
+
+coins = [
+    {'type': 'Quarter', 'state': 'California', 'year_released': 2005, 'date_of_statehood': 1850}
+]
