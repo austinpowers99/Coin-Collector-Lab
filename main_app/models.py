0bbee8e0d4ca3db78c0036e7706f185d810a32bb
austinpowers99 <samanthaf_9@outlook.com>
2023-07-26 20:41:52 -0700

    added delete/update functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Coin(models.Model):
+    type = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    year_released = models.IntegerField()
+    date_of_statehood = models.IntegerField()
+
+def __str__(self):
+    return f'{self.name} ({self.id})'
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'coin_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Coin
 
 # Create your views here.
+coins = [
+    {'type': 'Quarter', 'state': 'California', 'year_released': 2005, 'date_of_statehood': 1850}
+]
+
 def home(request):
     return render(request, 'home.html')
 
@@ -12,6 +18,23 @@ def coins_index(request):
         'coins': coins
 })
 
-coins = [
-    {'type': 'Quarter', 'state': 'California', 'year_released': 2005, 'date_of_statehood': 1850}
-]
+def coins_index(request):
+    coins = Coin.objects.all()
+    return render(request, 'coins/index.html', { 'coins': coins })
+
+def coins_detail(request, coin_id):
+    coin = Coin.objects.get(id=coin_id)
+    return render(request, 'coins/detail.html', { 'coin': coin })
+
+class CoinCreate(CreateView):
+    model = Coin
+    fields = '__all__'
+    success_url = '/coins/{coin_id}'
+
+class CoinUpdate(UpdateView):
+    model = Coin
+    field = ['type', 'state', 'year_released', 'date_of_statehood']
+
+class CoinDelete(DeleteView):
+    model = Coin
+    success_url = '/coins'
